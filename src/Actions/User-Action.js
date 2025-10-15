40fb10b7f354a14b76e26c42770031fdb9e4b288
VIXAY VONGPUNYA <vongpunyavixay@gmail.com>
2025-10-15 09:11:10 +0900

    allow creditentials

--- src/Actions/User-Action.js
+++ src/Actions/User-Action.js
@@ -5,7 +5,7 @@ axios.defaults.withXSRFToken = true;
 export const pendingLogin = (data) => async (dispatch) => {
   console.log("arrived");
 
-  await axios.get(
+  await server.get(
     `${process.env.REACT_APP_BACKEND_API_URL}/sanctum/csrf-cookie`
   );
   const response = await server.post("./login", data);
